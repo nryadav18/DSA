1943b526475214156da36c1df939889686ebf573
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-04 21:15:54 +0530

    Create Reverse Array2.cpp

---
+++ Arrays/Reverse Array2.cpp
@@ -0,0 +1,21 @@
+//Write a program to reverse the array
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    //nry_code();
+    int n;
+    cin>>n;
+    int arr[n];
+    for (int i=0;i<n;i++){
+        cin >> arr[i];
+    }
+    for (int i=0;i<n/2;i++){
+        int temp = arr[i];
+        arr[i] = arr[n-1-i];
+        arr[n-1-i] = temp;
+    }
+    for (int i=0;i<n;i++){
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
