220f5c631fc713cbf43496911e0cca09eb14ad31
Rajeswar Yadav <119839624+nryadav18@users.noreply.github.com>
2023-10-22 19:42:59 +0530

    Arrays_Intersection.cpp

---
+++ Arrays/Arrays_Intersection.cpp
@@ -0,0 +1,29 @@
+//Intersection of Two Arrays
+#include<bits/stdc++.h>
+using namespace std;
+int main (){
+    //nry_code();
+    int m,n;
+    cin>>m>>n;
+    int arr[m],brr[n];
+    for (int i=0;i<m;i++){
+        cin>>arr[i];
+    }
+    for (int i=0;i<n;i++){
+        cin>>brr[i];
+    }
+    int res[m+n];
+    for (int i=0;i<m;i++){
+        res[i] = arr[i];
+    }
+    for (int i=m;i<m+n;i++){
+        res[i] = brr[i-m];
+    }
+    sort(res,res+m+n);
+    for (int i=0;i<m+n;i++){
+        if (res[i]==res[i+1]){
+            cout << res[i] << " ";
+        }
+    }
+    return 0;
+}
